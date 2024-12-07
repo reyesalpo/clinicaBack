c0f9dbd37f3f3246713af2e80160f7093733ae6a
reyesmoreno <reyesmoreno606@gmail.com>
2024-12-07 14:21:04 -0600

    feat: add Entities folder and remove uarnings in the api and in the libraries

---
+++ DoctorAppBackEnd/Models/Entidades/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Entidades
+{
+    public class Usuario
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
